c2fa77e77393c8b2c92426c56b101396b047cefa
larissawithers <72356847+larissawithers@users.noreply.github.com>
2021-04-25 04:08:30 -0400

    Pages Added
    Added pages:
    Portfolio
    Home

--- script.js
+++ script.js
@@ -1,22 +1,187 @@
-$(document).on("click", "#menu_about", function(){
+.logo, img
+{
+    width: 100px;
+    float: left;
+}
 
-	$("#content_about").fadeIn("slow");
-  $("#content_about").siblings().fadeOut("fast");
+.profilePic
+{
+    width: 300px;
+}
 
-});
+.image
+{
+    width: 80%;
+    
+}
 
-$(document).on("click", "#menu_education", function(){
+nav
+{
+    padding-top: 40px;
+}
 
-	$("#content_education").fadeIn("slow");
-  $("#content_education").siblings().fadeOut("fast");
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #fff;
+    top: 0;
+    width: 100%;
+  }
+  
+li {
+    float: left;
+  }
+  
+li a {
+    display: block;
+    color: #9CB6D9;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  
+li a:hover:not(.active) {
+    background-color: transparent;
+    color: #466C8C;
+  }
+  
+.active {
+    color: black;
+    font-weight: bold;
+  }
 
-});
+#container
+{
+    width: 1000px;
+    border: 1px solid #ccc;
+    margin-left: auto;
+    margin-right: auto;
+    font-family: Helvetica, Arial, sans-serif;
+}
 
-$(document).on("click", "#menu_contact", function(){
+.box 
+{
+    padding-left: 800px;
+}
 
-	$("#content_contact").fadeIn("slow");
-  $("#content_contact").siblings().fadeOut("fast");
+.left {
+    float: left;
+    width: 500px;
+    height: 150px;
+    padding-top: 20px;
+    font-family: 'Noto Serif', serif;     
+}
 
-});
+.middle
+{
+    float: center;
+}
 
-$("#menu_about").click();
+.right {
+    float: right;
+    width:500px;
+    height: 150px;
+    padding-top: 20px;
+    padding-right: 0px;
+    font-family: 'Noto Serif', serif;
+}
+
+#header
+{
+    background-color: #A67E7B;
+    color: white;
+    padding: 10px;
+    font-weight: bold;
+}
+
+#menu
+{
+    padding: 5px;
+    border-bottom: 1px solid #ccc;
+}
+
+#menu a
+{
+    color: black;
+    text-decoration: none;
+    margin-left: 5px;
+    padding: 45px;
+}
+
+#portfolio
+{
+    width: 1000px;
+}
+
+#portfolio a
+{
+    color: black;
+    text-decoration: none;
+    margin-left: 5px;
+    padding: 45px;
+}
+
+.text
+{
+    background-color: #fcc5b2;
+    font-size: 12px;
+    padding: 5px;
+    display: none;
+}
+
+h1 {
+    font-size: 15px;
+}
+
+
+.row {
+    margin: 20px 20px;
+  }
+
+.row,
+  .row > .column {
+    padding: 20px;
+  }
+  
+  .column {
+    float: left;
+    width: 33.33%;
+  }
+  
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  h1{
+    font-family: 'Ultra', serif;
+    font-size: 80px;
+    padding-bottom: 5%;
+    padding-top: 5%;
+  }
+
+  h2{
+    font-family: 'Ultra', serif;
+    font-size: 25px;
+  }
+
+  h3{
+    font-family: 'Overlock', cursive;
+    font-size: 20px;
+  }
+
+  h4{
+    font-family: 'Overlock', cursive;
+    font-size: 20px;
+  }
+
+  p{
+    font-family: 'Overlock', cursive;
+    font-size: 30px;
+    padding-bottom: 5%;
+    padding-right: 15%;
+
+  }
